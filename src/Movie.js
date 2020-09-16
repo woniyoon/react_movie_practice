6721b4a546cba199fc910b2fda2c3bfadca5489c
woniyoon <gellifisch@gmail.com>
2020-09-16 22:18:33 +0900

    Add propTypes of genres

--- src/Movie.js
+++ src/Movie.js
@@ -22,6 +22,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
