8fd7b10f517331e5afacccaf64f3cfb2fbe33399
woniyoon <gellifisch@gmail.com>
2020-09-16 22:13:40 +0900

    Display movies' information

--- src/Movie.js
+++ src/Movie.js
@@ -3,7 +3,16 @@ import PropTypes from "prop-types";
 
 function Movie ({ id, year, title, summary, poster }){
     console.log(id);
-    return (<h5>{title}</h5>);
+    return (
+        <div class="movies_movie">
+            <img src={poster} alt={title} title={title} />
+            <div class="movie_data">
+                <h3 class="movie_title">{title}</h3>
+                <h5 class="movie_year">{year}</h5>
+                <p class="movie_summary">{summary}</p>
+            </div>
+        </div>
+    );
 }
 
 Movie.propTypes = {
