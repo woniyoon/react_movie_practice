28a6b980a0408f9f42a42628b0d40dacac21eadb
woniyoon <gellifisch@gmail.com>
2020-09-16 18:40:46 +0900

    Add condition to set Loading message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  // data that will change should be stored in state
+  state = {
+    isLoading: true,
+    movies: [],
+  }
+
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({ isLoading: false });
+    }, 5000);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+
+    return ( isLoading ? "Loading..." : "Ready");
+  }
 }
 
 export default App;
